147b7941bc8b924c6c6b633904477e4ed95f84e8
Sonia F <64592264+soniadotcom@users.noreply.github.com>
2020-09-07 17:26:36 +0300

    div_2: 4 button

--- dist/script.js
+++ dist/script.js
@@ -9,48 +9,6 @@ function upload(){
   image.drawTo(can);
 }
 
-function change1(){
-  if(imageIsLoaded(startImage)){
-    var can = document.getElementById("can");
-    for(var pixel of image.values()){
-      pixel.setRed((pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3);
-        pixel.setGreen((pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3);
-        pixel.setBlue((pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3);
-    }
-    image.drawTo(can);
-  }
-}
-
-function change2(){
-  if(imageIsLoaded(startImage)){
-    var can = document.getElementById("can");
-    for(var pixel of image.values()){
-      pixel.setGreen(255);
-    }
-    image.drawTo(can);
-  }
-}
-
-function change3(){
-  if(imageIsLoaded(startImage)){
-    var can = document.getElementById("can");
-    var avg = 0;
-    for(var pixel of image.values()){
-      avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
-      if(avg < 128){
-        pixel.setRed(2*avg);
-        pixel.setGreen(0);
-        pixel.setBlue(0);
-      } else {
-        pixel.setRed(255);
-        pixel.setGreen(2*avg-255);
-        pixel.setBlue(2*avg-255);
-      }
-    }
-    image.drawTo(can);
-  }
-}
-
 function change4(){
   if(imageIsLoaded(startImage)){
     var can = document.getElementById("can");
@@ -74,83 +32,6 @@ function change4(){
         pixel.setRed(255);
         pixel.setGreen(255);
         pixel.setBlue(255);
-      }
-    }
-    image.drawTo(can);
-  }
-}
-
-function change5(){
-  if(imageIsLoaded(startImage)){
-    var can = document.getElementById("can");
-    var avg = 0;
-    for(var pixel of image.values()){
-      avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
-      if(pixel.y <= can.height / 6){  //red
-        if(avg < 128){
-          pixel.setRed(2*avg);
-          pixel.setGreen(0);
-          pixel.setBlue(0);
-        } else {
-          pixel.setRed(255);
-          pixel.setGreen(2*avg-255);
-          pixel.setBlue(2*avg-255);
-        }
-      }
-      if(pixel.y > can.height / 6 && pixel.y <= 2 * can.height / 6){
-        if(avg < 128){  //orange
-          pixel.setRed(2*avg);
-          pixel.setGreen(0.8*avg);
-          pixel.setBlue(0);
-        } else {
-          pixel.setRed(255);
-          pixel.setGreen(1.2*avg-51);
-          pixel.setBlue(2*avg-255);
-        }
-      }
-      if(pixel.y > 2 * can.height / 6 && pixel.y <= 3 * can.height / 6){
-        if(avg < 128){  //yellow
-          pixel.setRed(2*avg);
-          pixel.setGreen(2*avg);
-          pixel.setBlue(0);
-        } else {
-          pixel.setRed(255);
-          pixel.setGreen(255);
-          pixel.setBlue(2*avg-255);
-        }
-      }
-      if(pixel.y > 3 * can.height / 6 && pixel.y <= 4 * can.height / 6){
-        if(avg < 128){  //green
-          pixel.setRed(0);
-          pixel.setGreen(2*avg);
-          pixel.setBlue(0);
-        } else {
-          pixel.setRed(2*avg-255);
-          pixel.setGreen(255);
-          pixel.setBlue(2*avg-255);
-        }
-      }
-      if(pixel.y > 4 * can.height / 6 && pixel.y <= 5 * can.height / 6){
-        if(avg < 128){  //blue
-          pixel.setRed(0);
-          pixel.setGreen(0);
-          pixel.setBlue(2*avg);
-        } else {
-          pixel.setRed(2*avg-255);
-          pixel.setGreen(2*avg-255);
-          pixel.setBlue(255);
-        }
-      }
-      if(pixel.y > 5 * can.height / 6){
-        if(avg < 128){  //violet
-          pixel.setRed(1.6*avg);
-          pixel.setGreen(0);
-          pixel.setBlue(1.6*avg);
-        } else {
-          pixel.setRed(0.4*avg+153);
-          pixel.setGreen(2*avg-255);
-          pixel.setBlue(0.4*avg+153);
-        }
       }
     }
     image.drawTo(can);
